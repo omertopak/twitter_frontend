baf210d9b51e184afc52bfda97d0b478e2143530
omer <omertopak1@gmail.com>
2024-05-12 13:52:02 +0100

    up

--- src/components/Replies.jsx
+++ src/components/Replies.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Box, Button, Typography, IconButton, TextField } from "@mui/material";
+import { Avatar, Box, Button, Typography, IconButton } from "@mui/material";
 import { bracketter, CircleButton } from "../styles/theme";
 import { iconAndText1 } from "../styles/theme";
 import { iconAndText2 } from "../styles/theme";
@@ -18,47 +18,31 @@ import IosShareIcon from "@mui/icons-material/IosShare";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import Replies from "./Replies";
 
-const TwitDetail = () => {
+const Replies = () => {
     return (
-      <Box>
-        <Box
-          display="flex"
-          sx={[
-            bracketter,
-            {
-              backgroundColor: "white",
-              position: "sticky",
-              top: "0",
-              zIndex: "1",
-              alignItems: "center",
-            },
-          ]}
-        >
-          <IconButton sx={{ margin: "5px", marginLeft: "15px" }}>
-            <ArrowBackIcon fontSize="medium" />
-          </IconButton>
-          <Typography sx={{ marginLeft: "15px" }}>Post</Typography>
-        </Box>
-
-        <Box sx={[{ display: "flex" },bracketter]}>
-          {/* //!avatari ekle */}
-          <Avatar
+      <Box sx={[{display:'flex'},bracketter]}>
+        <Box sx={[{ display: "flex" },]}>
+          <Box>
+             <Avatar
             alt="X"
             src={{}}
             sx={{ width: "2rem", height: "2rem", margin: "1rem" }}
-          />
-          <Box>
-            <Typography variant="subtitle1" component="h6">
+          /> 
+           </Box>
+           
+          </Box>
+      
+        <Box > 
+        <Box sx={{display:'flex'}}>
+             <Typography variant="subtitle1" component="h6">
               Ozgur demirtas
             </Typography>
             <Typography variant="subtitle1" component="h6" color="gray">
               @Profdemirtas
             </Typography>
-          </Box>
         </Box>
-        <Box sx={[{ justifyContent: "space-evenly" },bracketter]} padding={2}>
+           
           <Typography>
             TWIt Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Quidem deserunt quaerat veniam voluptatem vitae error inventore,
@@ -66,8 +50,6 @@ const TwitDetail = () => {
             ducimus tempore temporibus animi! Ad esse reiciendis minima hic
             molestias? Dolorem voluptatum quam nemo reiciendis quae quasi velit?
           </Typography>
-          <Typography>time and date</Typography>
-
           <Box sx={{ display: "flex", justifyContent: "space-between" }}>
             <IconButton sx={[iconAndText1]}>
               <ChatBubbleOutlineIcon fontSize="small"></ChatBubbleOutlineIcon>
@@ -89,49 +71,9 @@ const TwitDetail = () => {
               <IosShareIcon fontSize="small"></IosShareIcon>
             </IconButton>
           </Box>
-        </Box >
-        <Box sx={[bracketter]}>
-            <Box sx={{display:'flex'}}>
-            <Avatar
-            alt="X"
-            src={{}}
-            sx={{ width: "2rem", height: "2rem", margin: "1rem" }}
-          /> 
-           <TextField 
-         id="standard-basic"  
-        variant="standard"
-        placeholder="What is happening?!"
-        fullWidth 
-        multiline
-        rows={2}
-        sx={{marginTop:'10px',paddingRight:'1rem'}} />
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                 <Button  variant='contained' sx={{borderRadius:'20px',margin:'10px'}}>
-            Reply
-        </Button>
-            </Box>
-       
-        </Box>
-       
-        <Box sx={[{ display: "flex" },bracketter]}>
-            <Replies/>
         </Box>
       </Box>
     );
 };
 
-export default TwitDetail;
-
-// tweet:
-// image:
-// user:
-// repliedTo:
-// replies:
-// reply_count:
-// reposted_by:
-// repost_count:
-// tweet_viewers:
-// tweet_view_count:
-// favorites:
-// favorite_count:
+export default Replies;
