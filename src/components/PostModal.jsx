bfd790e570295c1fbd079d7f4553aad617b71431
omer <omertopak1@gmail.com>
2024-06-29 02:24:12 +0100

    up

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -16,7 +16,13 @@ export default function PostModal() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  
+  const CHARACTER_LIMIT = 100;
+  const [values, setValues] = React.useState({
+    tweet: ""
+  });
+  const handleChange = tweet => event => {
+    setValues({ ...values, [tweet]: event.target.value });
+  };
   return (
     <div>
       <Button  
@@ -31,40 +37,7 @@ export default function PostModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        {/* <Box sx={modal}>
-          <Box sx={{display:'flex'}}>
-        <Avatar alt="X" src={{}} sx={{ width: '2rem', height: '2rem', margin:'1rem' }}/>
-        <TextField 
-        id="standard-basic"  
-        variant="standard"
-        placeholder="What is happening?!"
-        fullWidth 
-        multiline
-        rows={2}
-        sx={{marginTop:'10px',paddingRight:'1rem'}} />
-        </Box>
-        <Box sx={{display:'flex',justifyContent:'space-between',marginTop:'10px',marginBottom:'10px'}}>
-        <Box >
-        <IconButton >
-            <AddPhotoAlternateIcon fontSize='small'></AddPhotoAlternateIcon>
-        </IconButton>
-        <IconButton >
-            <EmojiEmotionsIcon fontSize='small'></EmojiEmotionsIcon>
-        </IconButton>
-        <IconButton >
-            <GifIcon fontSize='medium'></GifIcon>
-        </IconButton>
-        <IconButton >
-            <LocationOnIcon fontSize='medium'></LocationOnIcon>
-        </IconButton>
-        </Box>
-        <Box sx={{marginRight:'1rem'}}>
-          <Button  variant='contained' sx={{borderRadius:'20px'}}>
-            Post
-          </Button>
-        </Box>
-        </Box>
-        </Box> */}
+       
           <Box sx={modal}>
       <Box> 
         <Avatar alt="X" src={{}} sx={{ width: '2rem', height: '2rem', margin:'1rem' }}/>
@@ -72,6 +45,12 @@ export default function PostModal() {
    
     <Box width='100%'>
         <TextField 
+        inputProps={{
+          maxlength: CHARACTER_LIMIT
+        }}
+        value={values.tweet}
+        helperText={`${values.tweet.length}/${CHARACTER_LIMIT}`}
+        onChange={handleChange("tweet")}
         id="standard-basic"  
         variant="standard"
         placeholder="What is happening?!"
@@ -108,3 +87,41 @@ export default function PostModal() {
     </div>
   );
 }
+
+
+
+
+ {/* <Box sx={modal}>
+          <Box sx={{display:'flex'}}>
+        <Avatar alt="X" src={{}} sx={{ width: '2rem', height: '2rem', margin:'1rem' }}/>
+        <TextField 
+        id="standard-basic"  
+        variant="standard"
+        placeholder="What is happening?!"
+        fullWidth 
+        multiline
+        rows={2}
+        sx={{marginTop:'10px',paddingRight:'1rem'}} />
+        </Box>
+        <Box sx={{display:'flex',justifyContent:'space-between',marginTop:'10px',marginBottom:'10px'}}>
+        <Box >
+        <IconButton >
+            <AddPhotoAlternateIcon fontSize='small'></AddPhotoAlternateIcon>
+        </IconButton>
+        <IconButton >
+            <EmojiEmotionsIcon fontSize='small'></EmojiEmotionsIcon>
+        </IconButton>
+        <IconButton >
+            <GifIcon fontSize='medium'></GifIcon>
+        </IconButton>
+        <IconButton >
+            <LocationOnIcon fontSize='medium'></LocationOnIcon>
+        </IconButton>
+        </Box>
+        <Box sx={{marginRight:'1rem'}}>
+          <Button  variant='contained' sx={{borderRadius:'20px'}}>
+            Post
+          </Button>
+        </Box>
+        </Box>
+        </Box> */}
